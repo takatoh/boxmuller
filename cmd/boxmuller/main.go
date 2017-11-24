035ecd97c7133f745d8b6541b6d17302855ea368
takatoh <takatoh.m@gmail.com>
2017-11-24 10:41:11 +0900

    cmd/boxmuller/main.go: -mean option.

--- cmd/boxmuller/main.go
+++ cmd/boxmuller/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
-	"os"
+//	"os"
+	"flag"
 	"strconv"
 
 	"github.com/takatoh/boxmuller"
 )
 
 func main() {
-	n, _ := strconv.Atoi(os.Args[1])
+	opt_mean := flag.Float64("mean", 0.0, "specify mean value.")
+	flag.Parse()
+
+	n, _ := strconv.Atoi(flag.Args()[0])
 	m := n / 2
 
-	bm := boxmuller.NewBoxMuller(1.0, 0.2)
+	bm := boxmuller.NewBoxMuller(*opt_mean, 0.2)
 	for i := 0; i < m; i++ {
 		z1, z2 := bm.Rand()
 		fmt.Println(z1)
