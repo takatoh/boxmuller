d998b431894cf41024d2b0bc4cb1a2a1bfcff493
takatoh <takatoh.m@gmail.com>
2017-11-24 10:45:24 +0900

    cmd/boxmuller/main.go: -variance option.

--- cmd/boxmuller/main.go
+++ cmd/boxmuller/main.go
@@ -11,12 +11,13 @@ import (
 
 func main() {
 	opt_mean := flag.Float64("mean", 0.0, "specify mean value.")
+	opt_variance := flag.Float64("variance", 1.0, "specify variance.")
 	flag.Parse()
 
 	n, _ := strconv.Atoi(flag.Args()[0])
 	m := n / 2
 
-	bm := boxmuller.NewBoxMuller(*opt_mean, 0.2)
+	bm := boxmuller.NewBoxMuller(*opt_mean, *opt_variance)
 	for i := 0; i < m; i++ {
 		z1, z2 := bm.Rand()
 		fmt.Println(z1)
