6a9f5773c931ab2daf7f7a8e169ac3f82e1971d5
takatoh <takatoh.m@gmail.com>
2017-11-24 10:29:14 +0900

    cmd/boxmuller/main.go: The number to be generated is given from the command line.

--- cmd/boxmuller/main.go
+++ cmd/boxmuller/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/takatoh/boxmuller"
 )
 
 func main() {
+	n, _ := strconv.Atoi(os.Args[1])
+	m := n / 2
+
 	bm := boxmuller.NewBoxMuller(1.0, 0.2)
-	for i := 0; i < 5000; i++ {
+	for i := 0; i < m; i++ {
 		z1, z2 := bm.Rand()
 		fmt.Println(z1)
 		fmt.Println(z2)
+	}
+	if n % 2 == 1 {
+		z1, _ := bm.Rand()
+		fmt.Println(z1)
 	}
 }
